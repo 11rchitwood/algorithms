c249b5beadd1e5ad3f2453d98995b2d7f80aa3d7
Ryan Chitwood <26073857+11rchitwood@users.noreply.github.com>
2022-06-27 15:29:35 -0400

    Update test-rotate.R

--- tests/testthat/test-rotate.R
+++ tests/testthat/test-rotate.R
@@ -1,4 +1,6 @@
 test_that("rotate works", {
-  expect_equal(rotate(1:5, 1), c(5, 1:4))
-  expect_equal(rotate(1:5, -1), c(2:5, 1))
+  expect_equal(rotate(1:5, 2), c(2, 1, 3, 4, 5))
+  expect_equal(rotate(1:5, -2), c(1, 3, 4, 5, 2))
+  expect_equal(rotate(1:5, 2:4), c(2, 3, 4, 1, 5))
+  expect_equal(rotate(1:5, -2:-4), c(1, 5, 2, 3, 4))
 })
