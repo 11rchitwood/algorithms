eafd1510f29a137fff365dd1c1c8b9849e04b666
Ryan Chitwood <chitwoodrw@icloud.com>
2025-02-24 16:15:32 -0500

    docs(rotate): Add docstring

--- R/rotate.R
+++ R/rotate.R
@@ -1,3 +1,14 @@
+#' Rotate a vector
+#'
+#' @param x a vector
+#' @param k an integer, the position to rotate
+#'
+#' @return a vector
+#'
+#' @examples
+#' rotate(1:7, 3)
+#'
+#' @export
 rotate <- function(x, k) {
   n <- length(x)
   x <- rev(x)
